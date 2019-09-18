ba9b2cb4ca267de8c97a675e4c5d5c667e4cfe3c
yangdongbjcn <yangdongbjcn@hotmail.com>
2019-09-18 09:51:12 +0800

    Add files via upload

---
+++ web/common/includes/menu_top.php
@@ -0,0 +1,29 @@
+<!-- container-fluid -->
+<div class='container-fluid'>
+<!-- row-fluid -->
+<div class='row-fluid'>
+
+	<!-- top menu starts -->
+	<div class='navbar navbar-default' role='navigation'>
+			
+		<ul class='nav navbar-nav'>
+			<li><a href='https://github.com/yangdongbjcn/webpage-includer'>Visit in GitHub</a></li>
+
+			<li class='drop-down' >
+				<a class='dropdown-toggle' data-toggle='dropdown'>
+					<span class='icon-user'></span>
+					User Help
+					<span class='caret'></span>
+				</a>
+				<ul class='dropdown-menu'>
+					<li><a href='https://github.com/yangdongbjcn/webpage-includer'>Visit in GitHub</a></li>
+				</ul>
+			</li>
+		</ul>
+		
+	</div>
+	<!-- top menu ends -->
+</div>
+<!-- row-fluid -->
+
+<script src="<?php echo $g_comn ?>includes/menu_top.js"></script>
---
+++ web/page/subpage/subsubpage/index.php
@@ -0,0 +1,12 @@
+<?php 
+	include('../../../basepath.php');	 
+	include('../../../common/includes/begin.php'); 
+	include('../../../common/includes/menu_top.php');
+?>
+	<link rel="stylesheet" href="<?php echo $g_subsubpage ?>index/content.css">
+	<script type="text/javascript" src="<?php echo $g_subsubpage ?>index/content.js"></script>
+<?php
+	include("index/content.php");
+	include('../../../common/includes/footer.php'); 
+	include('../../../common/includes/end.php');
+?>
